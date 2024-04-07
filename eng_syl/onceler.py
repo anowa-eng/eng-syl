94c08009ee496469e17ef638d2fda6796c9f074f
anova02 <anova02.regentshouse@gmail.com>
2024-04-06 19:21:28 -0700

    Optimize readability and performance

--- eng_syl/onceler.py
+++ eng_syl/onceler.py
@@ -12,30 +12,35 @@ import os
 class Onceler:
     def __init__(self, input_size=11, e2i='e2i_onc.pkl', latent_dim=500, embed_dim=500, max_feat=61):
         self.e2i = e2i
-        self.this_dir, this_filename = os.path.split(__file__)
+        self.this_dir = os.path.dirname(os.path.abspath(__file__))
         path_e2i = os.path.join(self.this_dir, e2i)
         path_clean = os.path.join(self.this_dir, 'clean_onc.pkl')
-        self.clean = pickle.load(open(path_clean, 'rb'))
-        self.e2i = pickle.load(open(path_e2i, 'rb'))
+        with open(path_clean, 'rb') as f:
+            self.clean = pickle.load(f)
+        with open(path_e2i, 'rb') as f:
+            self.e2i = pickle.load(f)
         self.embed_dim = embed_dim
         self.input_size = input_size
         self.latent_dim = latent_dim
         self.max_feat = max_feat + 1  # include dim for padding value 0 (no corresponding index in dict)
-        self.model = Sequential()
-        self.model.add(Input(shape=(input_size,)))
-        self.model.add(Embedding(self.max_feat, self.embed_dim))
-        self.model.add(Bidirectional(LSTM(self.latent_dim, return_sequences=True, recurrent_dropout=0.4),
-                                     input_shape=(input_size, 1)))
-        self.model.add(TimeDistributed(Dense(3)))
-        self.model.add(Activation('softmax'))
+        self.model = self.build_model()
         self.model.load_weights(os.path.join(self.this_dir, 'onceler_best_weights.h5'))
+
+    def build_model(self):
+        model = Sequential()
+        model.add(Input(shape=(self.input_size,)))
+        model.add(Embedding(self.max_feat, self.embed_dim))
+        model.add(Bidirectional(LSTM(self.latent_dim, return_sequences=True, recurrent_dropout=0.4)))
+        model.add(TimeDistributed(Dense(3)))
+        model.add(Activation('softmax'))
+        return model
 
     def ignore_class_accuracy(self, to_ignore=0):
         def ignore_accuracy(y_true, y_pred):
             y_true_class = K.argmax(y_true, axis=-1)
             y_pred_class = K.argmax(y_pred, axis=-1)
 
-            ignore_mask = K.cast(K.not_equal(y_pred_class, to_ignore), 'int32')
+            ignore_mask = K.cast(K.not_equal(y_true_class, 0), 'int32')
             matches = K.cast(K.equal(y_true_class, y_pred_class), 'int32') * ignore_mask
             accuracy = K.sum(matches) / K.maximum(K.sum(ignore_mask), 1)
             return accuracy
@@ -44,42 +49,30 @@ class Onceler:
 
     def fit(self, x_tr, y_tr, x_test, y_test, ep, batch_size, save_filename):
         self.model.compile(loss='categorical_crossentropy', optimizer='adam',
-                           metrics=['accuracy', self.ignore_class_accuracy(0)])
+                           metrics=['accuracy', self.ignore_class_accuracy()])
         es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=2)
-        ck = ModelCheckpoint(filepath=save_filename, monitor='accuracy', verbose=1, save_best_only=True,
+        ck = ModelCheckpoint(filepath=save_filename, monitor='val_accuracy', verbose=1, save_best_only=True,
                              mode='max')
-        Callbacks = [es, ck]
-        self.model.fit(x_tr, y_tr, epochs=ep, callbacks=Callbacks, batch_size=batch_size,
+        callbacks = [es, ck]
+        self.model.fit(x_tr, y_tr, epochs=ep, callbacks=callbacks, batch_size=batch_size,
                        validation_data=(x_test, y_test))
 
     def onc_split(self, word):
-        if self.in_data(word):
-            return self.clean[word]
-        inted = []
-        for c in word.lower():
-            inted += [self.e2i[c]]
-        inted = pad_sequences([inted], maxlen=self.input_size, padding='post')[0]
-        predicted = self.model.predict(inted.reshape(1, self.input_size, 1), verbose=0)[0]
-        converted = self.to_ind(predicted)
-
-        return (self.insert_syl(word, converted))
-
-    def in_data(self,word):
         if word in self.clean:
-            return True
+            return self.clean[word]
         else:
-            return False
+            inted = [self.e2i[c] for c in word.lower()]
+            inted = pad_sequences([inted], maxlen=self.input_size, padding='post')[0]
+            predicted = self.model.predict(np.array([inted]), verbose=0)[0]
+            converted = self.to_ind(predicted)
+            return self.insert_syl(word, converted)
 
     def to_ind(self, sequence):
-        index_sequence = []
-        for ind in sequence:
-            index_sequence += [np.argmax(ind)]
-        return index_sequence
+        return [np.argmax(ind) for ind in sequence]
 
     def insert_syl(self, word, indexes):
         index_list = np.where(np.array(indexes) == 2)[0]
-        word_array = [*word]
-        for i in range(0, len(index_list)):
-            word_array.insert(index_list[i] + i + 1, '-')
+        word_array = list(word)
+        for i, index in enumerate(index_list):
+            word_array.insert(index + i + 1, '-')
         return ''.join(word_array)
-
--- eng_syl/phonify.py
+++ eng_syl/phonify.py
@@ -1,40 +1,39 @@
-from tensorflow.keras.layers import LSTM, Dense, TimeDistributed, Bidirectional, Input, Embedding, Activation, GRU,concatenate, ZeroPadding1D, Lambda, Reshape
+import os
+import pickle
+import numpy as np
+from tensorflow.keras.layers import LSTM, Dense, TimeDistributed, Bidirectional, Input, Embedding, Activation, GRU, ZeroPadding1D
 from tensorflow.keras.models import Model
-from tensorflow.keras import regularizers
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras import backend as K
-import pickle
-import numpy as np
-import os
+from tensorflow.keras import regularizers
 
 class onc_to_phon:
-    def __init__(self,e2i ='op_e2i.pkl', d2i= 'op_d2i.pkl', input_size=19, latent_dim=128, embed_dim=500):
-        self.this_dir, this_filename = os.path.split(__file__)
+    def __init__(self, e2i='op_e2i.pkl', d2i='op_d2i.pkl', input_size=19, latent_dim=128, embed_dim=500):
+        self.this_dir = os.path.dirname(os.path.abspath(__file__))
         path_e2i = os.path.join(self.this_dir, e2i)
         path_d2i = os.path.join(self.this_dir, d2i)
-        internal_d2i = pickle.load(open(path_d2i, 'rb'))
+        with open(path_d2i, 'rb') as f:
+            internal_d2i = pickle.load(f)
         self.i2d = {value: key for key, value in internal_d2i.items()}
-        self.e2i = pickle.load(open(path_e2i, 'rb'))
+        with open(path_e2i, 'rb') as f:
+            self.e2i = pickle.load(f)
         self.input_size = input_size
         self.latent_dim = latent_dim
         self.embed_dim = embed_dim
         self.max_feat_e = len(self.e2i) + 1
         self.max_feat_d = len(internal_d2i) + 1
         self.model = self.build_model()
-        self.model.load_weights(os.path.join(self.this_dir,'op_best_weights.h5'))
+        self.model.load_weights(os.path.join(self.this_dir, 'op_best_weights.h5'))
 
     def build_model(self):
-
-        # orthographic and ipa input layers
         ortho_inputs = Input(shape=(self.input_size,))
-        # first branch ortho
         x = Embedding(self.max_feat_e, self.embed_dim)(ortho_inputs)
         x = Bidirectional(GRU(self.latent_dim, return_sequences=True, recurrent_dropout=0.2,
-                              activity_regularizer=regularizers.l2(1e-5)), input_shape=(self.input_size, 1))(x)
+                              activity_regularizer=regularizers.l2(1e-5)))(x)
         x = Bidirectional(GRU(self.latent_dim, return_sequences=True, recurrent_dropout=0.2,
-                              activity_regularizer=regularizers.l2(1e-5)), input_shape=(self.input_size, 1))(x)
-        x = ZeroPadding1D(padding=(0, self.input_size - x.shape[1]))(x)
+                              activity_regularizer=regularizers.l2(1e-5)))(x)
+        x = ZeroPadding1D(padding=(0, self.input_size))(x)
         z = TimeDistributed(Dense(self.max_feat_d))(x)
         z = Activation('softmax')(z)
 
@@ -43,18 +42,12 @@ class onc_to_phon:
         return model
 
     def ipafy(self, word):
-        inted_ortho = []
-        for c in word:
-            inted_ortho += [self.e2i[c]]
-
+        inted_ortho = [self.e2i[c] for c in word]
         inted_ortho = pad_sequences([inted_ortho], maxlen=self.input_size, padding='post')[0]
-        predicted = self.model.predict(inted_ortho.reshape(1, self.input_size, 1), verbose=0)[0]
+        predicted = self.model.predict(np.array([inted_ortho]), verbose=0)[0]
         indexes = self.to_ind(predicted)
         converted = [self.i2d[x] for x in indexes if x != 0]
         return converted
 
     def to_ind(self, sequence):
-        index_sequence = []
-        for ind in sequence:
-            index_sequence += [np.argmax(ind)]
-        return index_sequence
+        return [np.argmax(ind) for ind in sequence]
